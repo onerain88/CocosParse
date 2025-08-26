9a9c49424cec77d7390eab71c61ae8270d1c75dd
oneRain <onerain88@gmail.com>
2025-08-26 08:53:26 +0800

    fix: 适配 cocos 接口

--- assets/ParseJS/IndexedDBStorageController.ts
+++ assets/ParseJS/IndexedDBStorageController.ts
@@ -1,38 +1,22 @@
 /* global window */
 
-import { createStore, del, set, get, clear, keys } from 'idb-keyval';
-
-let IndexedDBStorageController: any;
-
-if (typeof window !== 'undefined' && window.indexedDB) {
-  try {
-    const ParseStore = createStore('parseDB', 'parseStore');
-
-    IndexedDBStorageController = {
-      async: 1,
-      getItemAsync(path: string) {
-        return get(path, ParseStore);
-      },
-      setItemAsync(path: string, value: string) {
-        return set(path, value, ParseStore);
-      },
-      removeItemAsync(path: string) {
-        return del(path, ParseStore);
-      },
-      getAllKeysAsync() {
-        return keys(ParseStore);
-      },
-      clear() {
-        return clear(ParseStore);
-      },
-    };
-  } catch (_) {
-    // IndexedDB not accessible
-    IndexedDBStorageController = undefined;
-  }
-} else {
-  // IndexedDB not supported
-  IndexedDBStorageController = undefined;
-}
+let IndexedDBStorageController: any = {
+  async: 1,
+  getItemAsync(path: string) {
+    return localStorage.getItem(path);
+  },
+  setItemAsync(path: string, value: string) {
+    return localStorage.setItem(path, value);
+  },
+  removeItemAsync(path: string) {
+    return localStorage.removeItem(path);
+  },
+  getAllKeysAsync() {
+    return null;
+  },
+  clear() {
+    return localStorage.clear();
+  },
+};
 
 export default IndexedDBStorageController;
